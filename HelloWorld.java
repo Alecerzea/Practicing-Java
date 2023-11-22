7b11e9ceffb1e33f322ee8da5e449034ec4fbe0d
Alecerzea <miguelcervanteszea@hotmail.com>
2023-11-22 14:18:44 -0500

    main

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
+        String name = "Alex";
         System.out.println("Hello, World!");
     }
 }
--- Variabes.java
+++
@@ -1,5 +0,0 @@
-public class Variabes {
-    public static void main(String[] args) {
-        String nombre = "Alex";
-    }
-}
